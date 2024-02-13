6b26007e0001391c413b922b014540957ca07465
AdryanPatrycky <adryanpatrycky87@gmail.com>
2024-02-13 17:51:19 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let Heroi = {
+
+    name: "horokussaky",
+    idade: 28,
+    tipo : "mago",
+    ataque: "magia"
+}
+
+console.log( `O ${Heroi.tipo} ${Heroi.name} atacou usando ${Heroi.ataque}` )
+
+
